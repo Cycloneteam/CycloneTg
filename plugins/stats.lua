9a84878eecb67b437490e34bcec7d30d56e89cdd
Cycloneteam <makancyclone@gmail.com>
2016-02-01 21:29:33 +0330

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -138,11 +138,11 @@ local function run(msg, matches)
 end
 return {
   patterns = {
-    "^[!/]([Ss]tats)$",
-    "^[!/]([Ss]tatslist)$",
-    "^[!/]([Ss]tats) (group) (%d+)",
-    "^[!/]([Ss]tats) (bot)",-- Put everything you like :)
-		"^[!/]([Cc]yclone)"-- Put everything you like :)
+    "^([Ss]tats)$",
+    "^([Ss]tatslist)$",
+    "^([Ss]tats) (group) (%d+)",
+    "^([Ss]tats) (bot)",-- Put everything you like :)
+		"^([Cc]yclone)"-- Put everything you like :)
     }, 
   run = run
 }
