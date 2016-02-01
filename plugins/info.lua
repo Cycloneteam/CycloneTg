40830c6a997021a55aee7e4536c48fbfe6cc4c84
Cycloneteam <makancyclone@gmail.com>
2016-02-01 15:04:52 +0330

    Update info.lua

--- plugins/info.lua
+++ plugins/info.lua
@@ -1,29 +1,61 @@
 do
-local function callback(extra, success, result)
-  vardump(success)
-  vardump(result)
+
+
+local function action_by_reply(extra, success, result)
+  local user_info = {}
+  local uhash = 'user:'..result.from.id
+  local user = redis:hgetall(uhash)
+  local um_hash = 'msgs:'..result.from.id..':'..result.to.id
+  user_info.msgs = tonumber(redis:get(um_hash) or 0)
+  user_info.name = user_print_name(user)..' ['..result.from.id..']'
+  local msgs = '6-messages sent : '..user_info.msgs
+  if result.from.username then
+    user_name = '@'..result.from.username
+  else
+    user_name = ''
+  end
+  local msg = result
+  local user_id = msg.from.id
+  local chat_id = msg.to.id
+  local user = 'user#id'..msg.from.id
+  local chat = 'chat#id'..msg.to.id
+  local data = load_data(_config.moderation.data)
+  if data[tostring('admins')][tostring(user_id)] then
+    who = 'Admim'
+  elseif data[tostring(msg.to.id)]['moderators'][tostring(user_id)] then
+    who = 'Moderator'
+  elseif data[tostring(msg.to.id)]['set_owner'] == tostring(user_id) then
+    who = 'Owner'
+  elseif tonumber(result.from.id) == tonumber(our_id) then
+    who = 'Group creator'
+  else
+    who = 'Member'
+  end
+  for v,user in pairs(_config.sudo_users) do
+    if user == user_id then
+      who = 'Sudo'
+    end
+  end
+  local text = 'نام کامل : '..(result.from.first_name or '')..' '..(result.from.last_name or '')..'\n'
+             ..'نام : '..(result.from.first_name or '')..'\n'
+             ..'نام خانوادگی : '..(result.from.last_name or '')..'\n'
+             ..'آیدی : '..user_name..'\n'
+             ..'شناسه : '..result.from.id..'\n'
+             ..msgs..'\n'
+             ..'مقام : '..who
+  send_large_msg(extra.receiver, text)
 end
 
-local function run(msg, matches)
-  local user = matches[1]
-    user = string.gsub(user," ","_")
-	if msg.to.type == 'chat' then
-    local chat = ''
-    return user
-  else 
-    return 'Only work in group'
-  end
+local function run(msg)
+   if msg.text == '!info' and msg.reply_id and is_momod(msg) then
+     get_message(msg.reply_id, action_by_reply, {receiver=get_receiver(msg)})
+   end
 end
 
 return {
-  description = "Users Information", 
-  usage = {
-    "/info (@user) : get id by username", },
-  patterns = {
-    "^[!/]info (.*)$",
-  }, 
-  run = run,
-  moderation = true 
+    patterns = {
+      '^[Ii]nfo$'
+    },
+  run = run
 }
-
 end
